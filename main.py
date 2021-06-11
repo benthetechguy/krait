ab74dfa792eae496ba48fdf2913d8d9e251be4bf
Ben Westover <kwestover.kw@gmail.com>
2021-06-10 23:28:23 -0400

    Add New Commands

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import discord
 import yaml
+import logging
+import random
 
-with open('config.yaml', 'r') as f:
-    config = yaml.load(f, Loader=yaml.BaseLoader)
+with open('config.yaml', 'r') as config_file:
+    config = yaml.load(config_file, Loader=yaml.BaseLoader)
 
 client = discord.Client()
 prefix = config['prefix']
@@ -18,5 +20,17 @@ async def on_message(message):
 
     if message.content.startswith(prefix+'ping'):
         await message.channel.send('Pong!')
+
+    if message.content.startswith(prefix+'flip'):
+        await message.channel.send(random.choice(["Heads", "Tails"]))
+
+    if message.content.startswith(prefix+'help'):
+        with open('commands.yaml', 'r') as commands_file:
+            commands = yaml.load(commands_file, Loader=yaml.BaseLoader)
+
+        embedVar = discord.Embed(title="Command List", color=0x00a0a0)
+        for command in commands:
+            embedVar.add_field(name=command, value=commands[command], inline=False)
+        await message.channel.send(embed=embedVar)
 
 client.run(config['token'])
